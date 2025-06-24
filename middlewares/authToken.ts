9565f5456e52f4ff8863c61343d2563ec5f712b0
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-06-24 15:20:19 +0100

    Fix some problems

--- middlewares/authToken.ts
+++ middlewares/authToken.ts
@@ -1,6 +1,5 @@
-import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from "express";
-import { UserTypes } from '../dto/user';
+import { verifyToken } from '../utils/jwt';
 
 /*---> Middleware to check if the token is valid <---*/
 export const authenticateToken = (req: Request, res: Response, next: NextFunction): any => {
@@ -8,11 +7,5 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     if (!token) {
         return res.status(401).json({ message: "Token not provided!" });
     }
-    jwt.verify(token, process.env.JWT_SECRET ?? '', (error: jwt.VerifyErrors | null, data: string | jwt.JwtPayload | undefined) => {
-        if (error) {
-            return res.status(403).json({ message: "Invalid token" });
-        }
-        req.data = data as UserTypes;
-        next();
-    })
+    verifyToken(token, req, res, next);
 }
--- models/file/index.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const fileSchema = new Schema({
-    url: { type: String, required: true },
-    type: { type: String, enum: ["image", "video", "audio", "document", "other"], required: true }
-}, { timestamps: true });
-
-const FileModel = mongoose.model("File", fileSchema);
-export default FileModel;
